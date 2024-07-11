e1c948bfc084e1aee567ec3382e69b2c24a8c036
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2024-07-11 16:12:08 -0500

    refactor (entrypoint.sh): parameterization

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export HOSTS="${HOSTS:-localhost,}"
+export VERBOSE="${VERBOSE:-False}"
 export PATH="$HOME/.asdf/bin:$HOME/.asdf/shims:$PATH"
 
 if [ "$PLUGIN_EXEC" = 'go version' ]; then
@@ -7,7 +9,19 @@ if [ "$PLUGIN_EXEC" = 'go version' ]; then
 fi
 
 # run playbook
-ansible-playbook -i localhost, -c local tests/${PLAYBOOK_FILE}
+cmd="ansible-playbook"
+declare -a args=(
+	"-i"
+	"$HOSTS"
+	"-c"
+	"local"
+	"tests/${PLAYBOOK_FILE}"
+)
+if [ "$VERBOSE" = "True" ]; then
+	args+=("-v")
+	export ANSIBLE_STDOUT_CALLBACK=debug
+fi
+"$cmd" "${args[@]}"
 
 # verify asdf was installed
 asdf --version
